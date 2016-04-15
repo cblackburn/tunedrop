03096ca6c676c6ecfe7c50978185a46003522cdc
Chris Blackburn <chris@midwiretech.com>
2016-04-15 10:46:39 -0500

    fix tests

---
+++ test/support/test_helpers.ex
@@ -0,0 +1,21 @@
+defmodule Tunedrop.TestHelpers do
+  alias Tunedrop.Repo
+
+  def insert_user(attrs \\ %{}) do
+    changes = Dict.merge(%{
+      email: "user@example.com",
+      username: "user#{Base.encode16(:crypto.rand_bytes(8))}",
+      password: "supersecret",
+      }, attrs)
+
+    %Tunedrop.User{}
+    |> Tunedrop.User.registration_changeset(changes)
+    |> Repo.insert!()
+  end
+
+  def insert_song(user, attrs \\ %{}) do
+    user
+    |> Ecto.build_assoc(:song, attrs)
+    |> Repo.insert!()
+  end
+end
